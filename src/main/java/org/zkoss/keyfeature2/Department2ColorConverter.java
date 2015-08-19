1bc0c6499866cd4c19d0ffe24326f40f59639612
hawk <hawkchen@potix.com>
2015-08-19 16:08:31 +0800

    add HTML box theme

---
+++ src/main/java/org/zkoss/keyfeature2/Department2ColorConverter.java
@@ -0,0 +1,29 @@
+package org.zkoss.keyfeature2;
+
+import org.zkoss.bind.BindContext;
+import org.zkoss.bind.Converter;
+import org.zkoss.zk.ui.Component;
+
+public class Department2ColorConverter implements Converter<String, Department, Component> {
+
+	public Department coerceToBean(String arg0, Component arg1, BindContext arg2) {
+		return null;
+	}
+
+	public String coerceToUi(Department department, Component component, BindContext context) {
+		switch (department) {
+		case ADMIN:
+			return "black";
+		case IT:
+			return "green";
+		case RD:
+			return "purple";
+		case SALES:
+			return "blue";
+		default:
+			return "";
+		}
+	}
+
+
+}
