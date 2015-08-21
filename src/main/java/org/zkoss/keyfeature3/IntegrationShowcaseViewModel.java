0931ceeb44b725bef44324ef0526fbc681739606
Robert Wenzel <robertwenzel@potix.com>
2015-08-21 12:34:18 +0800

    some more JS integration ideas

---
+++ src/main/java/org/zkoss/keyfeature3/IntegrationShowcaseViewModel.java
@@ -0,0 +1,19 @@
+package org.zkoss.keyfeature3;
+
+public class IntegrationShowcaseViewModel {
+
+	private boolean likesJava8 = true;
+	private String percentage = "33%";
+
+	public boolean isLikesJava8() {
+		return likesJava8;
+	}
+
+	public void setLikesJava8(boolean likesJava8) {
+		this.likesJava8 = likesJava8;
+	}
+
+	public String getPercentage() {
+		return percentage;
+	}
+}
