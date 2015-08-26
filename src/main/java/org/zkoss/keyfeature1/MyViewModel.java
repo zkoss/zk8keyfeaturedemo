27d7bdebdd3fc46bf684aa30ad3c09112e68d68d
Matthieu Duchemin <matthieu@potix.com>
2015-08-26 15:50:49 +0800

    updated message lenght

--- src/main/java/org/zkoss/keyfeature1/MyViewModel.java
+++ src/main/java/org/zkoss/keyfeature1/MyViewModel.java
@@ -28,13 +28,12 @@ public class MyViewModel {
 		myStats.add(new StatBlock("uniqueVisitorClick", "Unique Visitors", "1,291,922", "fa-users", "purple"));
 		myStats.add(new StatBlock("avgTimeClick", "Avg Time On Site", "00:12:23", "fa-clock-o", "red"));
 		
-		myMessages.add(new Message(new User("John Doe","./img/user-5.jpg"),"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id nunc non eros fermentum vestibulum ut id felis. Nunc molestie libero eget urna aliquet, vitae laoreet felis ultricies. Fusce sit amet massa malesuada, tincidunt augue vitae, gravida felis."));
-		myMessages.add(new Message(new User("Terry Ng","./img/user-6.jpg"),"Sed in ante vel ipsum tristique euismod posuere eget nulla. Quisque ante sem, scelerisque iaculis interdum quis, eleifend id mi. Fusce congue leo nec mauris malesuada, id scelerisque sapien ultricies."));
-		myMessages.add(new Message(new User("Fiona Log","./img/user-8.jpg"),"Pellentesque dictum in tortor ac blandit. Nulla rutrum eu leo vulputate ornare. Nulla a semper mi, ac lacinia sapien. Sed volutpat ornare eros, vel semper sem sagittis in. Quisque risus ipsum, iaculis quis cursus eu, tristique sed nulla."));
-		myMessages.add(new Message(new User("John Doe","./img/user-7.jpg"),"Morbi molestie lorem quis accumsan elementum. Morbi condimentum nisl iaculis, laoreet risus sed, porta neque. Proin mi leo, dapibus at ligula a, aliquam consectetur metus."));
+		myMessages.add(new Message(new User("John Doe","./img/user-5.jpg"),"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id nunc non eros fermentum vestibulum ut id felis."));
+		myMessages.add(new Message(new User("Terry Ng","./img/user-6.jpg"),"Sed in ante vel ipsum tristique euismod posuere eget nulla. Quisque ante sem, scelerisque iaculis interdum quis, eleifend id mi."));
+		myMessages.add(new Message(new User("Fiona Log","./img/user-8.jpg"),"Pellentesque dictum in tortor ac blandit. Nulla rutrum eu leo vulputate ornare. Nulla a semper mi, ac lacinia sapien."));
+		myMessages.add(new Message(new User("John Doe","./img/user-7.jpg"),"Morbi molestie lorem quis accumsan elementum. Morbi condimentum nisl iaculis, laoreet risus sed, porta neque."));
 		
 		scrollProperty.put("height", "200px");
-		scrollProperty.put("start", "bottom");
 		scrollProperty.put("scrollToEnd", ADD_NEW_MESSAGE);
 	}
 
