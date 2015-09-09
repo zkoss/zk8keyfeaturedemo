50e60177951516c39625f46755d73734e3150939
Vincent <vincentjian@potix.com>
2015-09-09 12:02:31 +0800

    change data source and using csvjdbc

--- src/main/java/org/zkoss/keyfeature4/ShadowRodVM.java
+++ src/main/java/org/zkoss/keyfeature4/ShadowRodVM.java
@@ -1,52 +1,134 @@
 package org.zkoss.keyfeature4;
 
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyCommand;
+import org.zkoss.bind.annotation.NotifyCommands;
 import org.zkoss.bind.annotation.SmartNotifyChange;
 import org.zkoss.bind.annotation.ToClientCommand;
 import org.zkoss.bind.annotation.ToServerCommand;
+import org.zkoss.zk.ui.Sessions;
 import org.zkoss.zul.ListModelList;
 
-@ToServerCommand(ShadowRodVM.LOAD_DATA_COMMAND)
-@ToClientCommand({ShadowRodVM.LOAD_DATA_COMMAND, ShadowRodVM.UPDATE_BEGIN})
-@NotifyCommand(value = ShadowRodVM.UPDATE_BEGIN, onChange="_vm_.begin")
+@ToServerCommand({ShadowRodVM.LOAD_DATA_COMMAND, ShadowRodVM.SHOW_PREVIEW_COMMAND, ShadowRodVM.SCROLL_INTO_VIEW_COMMAND})
+@ToClientCommand({ShadowRodVM.LOAD_DATA_COMMAND, ShadowRodVM.UPDATE_BEGIN, ShadowRodVM.UPDATE_PREVIEW, ShadowRodVM.UPDATE_SCROLL})
+@NotifyCommands({
+	@NotifyCommand(value = ShadowRodVM.UPDATE_BEGIN, onChange="_vm_.begin"),
+	@NotifyCommand(value = ShadowRodVM.UPDATE_PREVIEW, onChange="_vm_.preview"),
+	@NotifyCommand(value = ShadowRodVM.UPDATE_SCROLL, onChange="_vm_.begin"),
+})
 public class ShadowRodVM {
-	static final String UPDATE_BEGIN = "updateBegin";
+
+	private RestaurantService service = RestaurantService.getInstance(Sessions.getCurrent());
 	static final String LOAD_DATA_COMMAND = "loadData";
+	static final String UPDATE_BEGIN = "updateBegin";
+	static final String SHOW_PREVIEW_COMMAND = "showPreview";
+	static final String UPDATE_PREVIEW = "updatePreview";
+	static final String SCROLL_INTO_VIEW_COMMAND = "scrollIntoView";
+	static final String UPDATE_SCROLL = "updateScroll";
 	private int begin = 0;
 	private int cachedSize = 30;
 	private int totalSize;
-	private ListModelList<User> usersList;
+	private ListModelList<Restaurant> restaurantList;
+	private List<String> cities;
+	private List<RestaurantPreview> previewBar;
+	private RestaurantPreview preview;
+	private Map<String, Integer> numOfCityInit;
+	private int totalNumOfCityInit;
 
 	@Init
 	public void init() {
-		totalSize = UserService.getUsersCount();
-		usersList = new ListModelList<User>(loadUsers(0));  // simulate load from DB
+		totalSize = service.getRestaurantsCount();
+		restaurantList = new ListModelList<Restaurant>(loadRestaurant(0));
+		previewBar = service.countGroupByCity();
+		cities = service.getAllCities();
+		totalNumOfCityInit = cities.size();
 	}
 
 	@Command(LOAD_DATA_COMMAND)
 	@SmartNotifyChange({"begin"})
-	public void loadData(@BindingParam("loadingIndex") int loadingIndex, @BindingParam("direction") String direction) {
-		usersList.clear();
+	public void loadData(@BindingParam("loadingIndex") int index, @BindingParam("direction") String direction) {
+		restaurantList.clear();
 		if("down".equals(direction)) {
-			usersList.addAll(loadUsers(loadingIndex));
+			restaurantList.addAll(loadRestaurant(index));
 		} else if("up".equals(direction)) {
-			usersList.addAll(loadUsers(loadingIndex - cachedSize));
+			restaurantList.addAll(loadRestaurant(index - cachedSize));
 		}
 	}
 
-	private List<User> loadUsers(int index) {
-		this.begin = Math.min(Math.max(index, 0), totalSize - cachedSize);
-		int end = Math.min(begin + cachedSize, totalSize);
-		return UserService.getUsers(begin, end);
+	@Command(SCROLL_INTO_VIEW_COMMAND)
+	@SmartNotifyChange({"begin"})
+	public void scrollIntoView(@BindingParam("loadingIndex") int index, @BindingParam("direction") String direction) {
+		String cityName = cities.get(index);
+		int loadingIndex = 0;
+		for (RestaurantPreview rp : previewBar) {
+			if (cityName.equals(rp.getCityName())) {
+				break;
+			} else {
+				loadingIndex += rp.getNumberOfCity();
+			}
+		}
+		loadData(loadingIndex, direction);
 	}
 
-	public ListModelList<User> getUsers() {
-		return usersList;
+	@Command(SHOW_PREVIEW_COMMAND)
+	@SmartNotifyChange({"preview"})
+	public void showPreview(@BindingParam("showIndex") int index, @BindingParam("position") int pos) {
+		String cityName = cities.get(index);
+		List<Restaurant> rlist = service.getRestaurantsByCity(cityName);
+		Map<String, Integer> numberOfType = new TreeMap<String, Integer>();
+		for (Restaurant res : rlist) {
+			String type = res.getType();
+			Integer number = numberOfType.get(type);
+			if (number == null) {
+				numberOfType.put(type, new Integer(1));
+			} else {
+				numberOfType.put(type, ++number);
+			}
+		}
+		for (RestaurantPreview rp : previewBar) {
+			if (cityName.equals(rp.getCityName())) {
+				preview = rp;
+				preview.setNumberOfType(numberOfType);
+				preview.setPosition(pos);
+				break;
+			}
+		}
+	}
+
+	private List<Restaurant> loadRestaurant(int index) {
+		this.begin = Math.min(Math.max(index, 0), totalSize - cachedSize);
+		int end = Math.min(begin + cachedSize, totalSize);
+		return service.getRestaurants(begin, end);
+	}
+
+	public ListModelList<Restaurant> getRestaurantList() {
+		return restaurantList;
+	}
+
+	public Map<String, Integer> getNumOfCityInit() {
+		if (numOfCityInit == null) {
+			numOfCityInit = new TreeMap<String, Integer>();
+			for (RestaurantPreview rp : previewBar) {
+				String key = rp.getCityInitial();
+				Integer number = numOfCityInit.get(key);
+				if (number == null) {
+					numOfCityInit.put(key, 1);
+				} else {
+					numOfCityInit.put(key, ++number);
+				}
+			}
+		}
+		return numOfCityInit;
+	}
+
+	public int getTotalNumOfCityInit() {
+		return totalNumOfCityInit;
 	}
 
 	public int getBegin() {
@@ -56,4 +138,9 @@ public class ShadowRodVM {
 	public int getTotalSize() {
 		return totalSize;
 	}
+
+	public RestaurantPreview getPreview() {
+		return preview;
+	}
+
 }
--- src/main/webapp/keyfeature4/scripts/rod-shadow.js
+++ src/main/webapp/keyfeature4/scripts/rod-shadow.js
@@ -3,31 +3,115 @@ function initRodList(container, totalSize, rowHeight) {
 	var $topPadding = $container.find('> .topPadding');
 	var $bottomPadding = $container.find('> .bottomPadding');
 	var binder = zkbind.$(container);
-	
+
+	container.viewHeight = 600;
+	container.rowHeight = rowHeight;
+	container.loadData = function (index, direction) {
+		container.loadingPosition = $container.scrollTop();
+		binder.command('loadData', {loadingIndex: index, direction: direction});
+	}
+
 	$container.on('scroll', function(evt) {
 		if (container.loadingPosition) {
 			$container.scrollTop(container.loadingPosition);
 			return;
 		}
 		if ($bottomPadding.position().top < $container.height()) {
-			loadData(Math.floor($container.scrollTop() / rowHeight), 'down');
+			container.loadData(Math.floor($container.scrollTop() / rowHeight), 'down');
 		} else if ($topPadding.height() > $container.scrollTop()) {
-			loadData(Math.ceil(($container.scrollTop() + $container.height()) / rowHeight), 'up');
+			container.loadData(Math.ceil(($container.scrollTop() + $container.height()) / rowHeight), 'up');
 		}
 	});
-	
+
 	binder.after('loadData', function(pos) {
 		container.loadingPosition = null;
 	});
-	
+
 	binder.after('updateBegin', function(begin) {
 		var numLoadedRows = $container.children().length - 2; //subtract top/bottomPadding
 		$topPadding.height(begin * rowHeight);
 		$bottomPadding.height((totalSize - numLoadedRows - begin) * rowHeight);
 	});
+}
 
-	function loadData(index, direction) {
-		container.loadingPosition = $container.scrollTop();
-		binder.command('loadData', {loadingIndex: index, direction: direction});
+function initPreviewBar(previewBar, totalSize) {
+	var container = previewBar.$f('restaurantList');
+	var preview = previewBar.$f('preview');
+	var $container = jq(container.$n());
+	var $previewBar = jq(previewBar);
+
+	var index = 0;
+	for (var w = previewBar.firstChild; w; w = w.nextSibling) {
+		if (w.cityNumber) {
+			w.$n().innerHTML = index % 2 == 0 ? w.cityInit : '';
+			var h = Math.round(container.viewHeight * w.cityNumber / totalSize);
+			h = h == 0 ? 1 : h;
+			var style = w.$n().style;
+			style.height = h + 'px';
+			style.lineHeight = h + 'px';
+			index++;
+		}
 	}
+	preview.hide = function() {
+		preview.shallShow = false;
+		preview.$n().style.display = 'none';
+	};
+
+	var prevY = 0;
+	$previewBar.click(function(evt) {
+		var tg = evt.target;
+		var w = zk.Widget.$(tg.id);
+		if (w.cityNumber) {
+			var y = evt.offsetY;
+			if (y < 0) { 
+				return;
+			}
+			y += jq(tg).offset().top
+			var index = Math.floor(y * totalSize / container.viewHeight);
+			binder.command('scrollIntoView', {loadingIndex: index, direction: prevY < y ? 'down' : 'up'});
+			binder.after('updateScroll', function (begin) {
+				$container.scrollTop(begin * container.rowHeight);
+			});
+			prevY = y;
+		}
+		preview.hide();
+	});
+
+	$previewBar.mouseover(function(evt) {
+		preview.shallShow = true;
+	});
+
+	var binder = zkbind.$(previewBar), prevY = -1;
+	$previewBar.mousemove(function(evt) {
+		var y = evt.offsetY;
+		if (!preview.shallShow)
+			return;
+		if (y < 0)
+			return;
+		if (prevY == y)
+			return;
+		
+		var tg = evt.target;
+		var w = zk.Widget.$(tg.id);
+		if (w.cityNumber) {
+			prevY = y;
+			y += jq(tg).offset().top
+			if (y > container.viewHeight)
+				y = container.viewHeight;
+			var index = Math.floor(y * totalSize / container.viewHeight);
+			binder.command('showPreview', {showIndex: index, position: y});
+		}
+	});
+
+	$previewBar.mouseout(function(evt) {
+		preview.hide();
+	});
+
+	binder.after('updatePreview', function(view) {
+		if (view) {
+			var style = preview.$n().style;
+			style.display = 'block';
+			style.top = view.position + 'px';
+		}
+	});
 }
